bdb9bace383495b8e8febd45138c8e4fb2515864
Espen Moe-Nilssen <espen@medialog.no>
2015-11-02 16:07:53 +0100

    adding properies

--- medialog/iconpicker/behavior.py
+++ medialog/iconpicker/behavior.py
@@ -15,18 +15,7 @@ from plone import api
 from medialog.iconpicker.interfaces import IIconPickerSettings
 
 
-
-
-from plone.autoform import directives
-from plone.autoform.interfaces import IFormFieldProvider
-from plone.supermodel import model
-from zope.component import adapter
-from zope.interface import implementer
-from zope.interface import provider
-
-
-@provider(IFormFieldProvider)
-class IIconPickerBehavior(model.Schema):
+class IIconPickerBehavior(form.Schema):
     """ A field for icons"""
     
     
@@ -41,44 +30,12 @@ class IIconPickerBehavior(model.Schema):
             iconfield=IconPickerFieldWidget,
     )
     
-    
-#alsoProvides(IIconPickerBehavior, IFormFieldProvider)
-
-
-
-
-@implementer(IconPickerBehavior)
-class IconPickerBehavior(object):
-    """
-    properties to be used in listing views 
-    """
-    
-    def __init__(self, context):
-        self.context = context
-
     @property
-    def iconset(self):
+    def iconset():
         """Returns current iconset name This is also used for loading the resources below"""
         import pdb; pdb.set_trace()
         return api.portal.get_registry_record('medialog.iconpicker.interfaces.IIconPickerSettings.iconset')
-  
-    def family_css(self):
-        #return css_family_class, like fa, wi
-        iconset = self.iconset()
-        if iconset == 'glyphicon':
-             return 'glyphicon'
-        if iconset == 'mapicon':
-            return 'map-icons'
-        if iconset == 'typicon':
-            return 'typcn'
-        if iconset == 'ionicon':
-            return 'ionicons'
-        if iconset == 'weathericon':
-            return 'wi'
-        if iconset == 'octicon' :
-            return 'octicon'
-        if iconset == 'elusiveicon':
-            return 'el-icon'
-        
+    
+alsoProvides(IIconPickerBehavior, IFormFieldProvider)
 
-   
+
