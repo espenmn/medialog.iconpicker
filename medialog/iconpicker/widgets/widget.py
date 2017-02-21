357a61bea9aaef7d2ef365b514a00a34a5cc479d
Espen Moe-Nilssen <espen@medialog.no>
2017-02-21 19:49:07 +0100

    font / css

--- medialog/iconpicker/widgets/widget.py
+++ medialog/iconpicker/widgets/widget.py
@@ -71,7 +71,7 @@ class IconPickerWidget(text.TextWidget):
 
     def medialogfont(self):
         return """
-        <link rel="stylesheet" href="++resource++medialog.iconpicker/icon-fonts/medialogfont/font/medialogfont.css"/>
+        <link rel="stylesheet" href="++resource++medialog.iconpicker/icon-fonts/medialogfont/css/medialogfont.css"/>
         """
         
     def glyphicon(self):
